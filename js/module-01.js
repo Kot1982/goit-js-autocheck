a60730bdc9611cb97b1e2876cc956e52ca27583c
Kostiantyn Oliinyk <94046694+Kot1982@users.noreply.github.com>
2022-04-03 19:39:54 +0300

    add files

---
+++ js/module-01.js
@@ -0,0 +1,10 @@
+// Объяви две переменные, productName для названия товара и pricePerItem для хранения цены за штуку.
+// При объявлении присвой переменным следующие характеристики товара:
+// название - строка "Droid"
+// цена за штуку - число 2000
+
+const productName = 'Droid'
+const pricePerItem = 2000
+
+console.log(productName)
+console.log(pricePerItem) 
